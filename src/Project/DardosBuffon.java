00fd0bcbeb50dcfe081e1f6f5a553441dcdc7dd8
YovaInz <yovanniinzunza@gmail.com>
2024-03-23 18:24:10 -0700

    DardosBuffon.java update

--- src/Project/DardosBuffon.java
+++ src/Project/DardosBuffon.java
@@ -6,54 +6,51 @@ import java.util.Scanner;
 
 public class DardosBuffon extends JPanel {
     private static Scanner input = new Scanner(System.in);
-    private static int dardos = 0, skip = 0;
+    private static int dardos = 0;
     private static double hip;
 
     @Override
     protected void paintComponent(Graphics g) {
-        if (skip == 1) {
-            super.paintComponent(g);
-            int radio = 100, centroX = getWidth() / 2, centroY = getHeight() / 2, X, Y, totac = 0;
+        super.paintComponent(g);
+        int radio = 100, centroX = getWidth() / 2, centroY = getHeight() / 2, X, Y, totac = 0;
 
-            for (int i = 5; i > 0; i--) {
-                if (i % 2 == 0) {
-                    g.setColor(Color.WHITE);
-                } else {
-                    g.setColor(Color.BLACK);
-                }
-                g.fillOval(centroX - i * radio / 5, centroY - i * radio / 5, i * radio / 5 * 2, i * radio / 5 * 2);
+        for (int i = 5; i > 0; i--) {
+            if (i % 2 == 0) {
+                g.setColor(Color.WHITE);
+            } else {
+                g.setColor(Color.BLACK);
             }
-            centroX -= 5;
-            centroY -= 5;
-            // Dibujar un punto verde en el centro del tablero
-            g.setColor(Color.RED);
-            g.fillOval(centroX, centroY, 10, 10);
-
-            for (int i = 0; i < dardos; i++) {
-                g.setColor(Color.RED);
-                X = (int) (Math.random() * 100);
-                Y = (int) (Math.random() * 100);
-                if (i % 2 == 0) {
-                    X *= -1;
-                    Y *= -1;
-                } else if (i % 3 == 0)
-                    X *= -1;
-                else if (i % 5 == 0)
-                    Y *= -1;
-                hip = Math.sqrt((X * X) + (Y * Y));
-                if (hip <= 100) {
-                    totac++;
-                    System.out.println("total aciertos: " + totac + "\t hip: " + hip + "\t i: " + i);
-                    g.setColor(Color.BLUE);
-                }
-                g.fillOval(centroX + X, centroY + Y, 6, 6);
-
-            }
-
-            System.out.println("totac: " + totac + "\tn: " + dardos);
-            System.out.println("PI ~ " + ((double) totac / (double) dardos) * 4);
+            g.fillOval(centroX - i * radio / 5, centroY - i * radio / 5, i * radio / 5 * 2, i * radio / 5 * 2);
         }
-        skip++;
+        centroX -= 5;
+        centroY -= 5;
+        // Dibujar un punto verde en el centro del tablero
+        g.setColor(Color.RED);
+        g.fillOval(centroX, centroY, 10, 10);
+
+        for (int i = 0; i < dardos; i++) {
+            g.setColor(Color.RED);
+            X = (int) (Math.random() * 100);
+            Y = (int) (Math.random() * 100);
+            if (i % 2 == 0) {
+                X *= -1;
+                Y *= -1;
+            } else if (i % 3 == 0)
+                X *= -1;
+            else if (i % 5 == 0)
+                Y *= -1;
+            hip = Math.sqrt((X * X) + (Y * Y));
+            if (hip <= 100) {
+                totac++;
+                System.out.println("total aciertos: " + totac + "\t hip: " + hip + "\t i: " + i);
+                g.setColor(Color.BLUE);
+            }
+            g.fillOval(centroX + X, centroY + Y, 6, 6);
+
+        }
+
+        System.out.println("totac: " + totac + "\tn: " + dardos);
+        System.out.println("PI ~ " + ((double) totac / (double) dardos) * 4);
     }
 
     public static void main(String[] args) {
