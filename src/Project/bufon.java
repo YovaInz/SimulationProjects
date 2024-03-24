a7457ea1d16fa448e245eefaf80923cd2479dcf3
YovaInz <yovanniinzunza@gmail.com>
2024-03-23 19:11:07 -0700

    ya no se que hacer con mi vida

---
+++ src/Project/bufon.java
@@ -0,0 +1,177 @@
+package Project;
+
+import java.awt.*;
+import java.util.Scanner;
+import org.jfree.chart.*;
+import org.jfree.data.general.DefaultPieDataset;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+public class bufon extends JPanel {
+  private static Scanner leer = new Scanner(System.in);
+  private float n; // Almacena el número de agujas
+  private static double hip;
+  private static int skipAguja = 0, skip = 0, dardos = 0;
+
+  public bufon(int n) {
+    this.n = n; // Asigna el número de agujas a la variable de instancia
+    setBorder(new EmptyBorder(5, 5, 5, 5));
+    setBackground(Color.white);
+    setPreferredSize(new Dimension(800, 1000)); // Establece el tamaño preferido del panel
+  }
+
+  @Override
+  protected void paintComponent(Graphics g) {
+    if (skip == 1) {
+      super.paintComponent(g);
+      int radio = 100, centroX = getWidth() / 2, centroY = getHeight() / 2, X, Y, totac = 0;
+
+      for (int i = 5; i > 0; i--) {
+        if (i % 2 == 0) {
+          g.setColor(Color.WHITE);
+        } else {
+          g.setColor(Color.BLACK);
+        }
+        g.fillOval(centroX - i * radio / 5, centroY - i * radio / 5, i * radio / 5 * 2, i * radio / 5 * 2);
+      }
+      centroX -= 5;
+      centroY -= 5;
+      // Dibujar un punto verde en el centro del tablero
+      g.setColor(Color.RED);
+      g.fillOval(centroX, centroY, 10, 10);
+      for (int i = 0; i < dardos; i++) {
+        g.setColor(Color.RED);
+        X = (int) (Math.random() * 100);
+        Y = (int) (Math.random() * 100);
+        if (i % 2 == 0) {
+          X *= -1;
+          Y *= -1;
+        } else if (i % 3 == 0)
+          X *= -1;
+        else if (i % 5 == 0)
+          Y *= -1;
+        hip = Math.sqrt((X * X) + (Y * Y));
+        if (hip <= 100) {
+          totac++;
+          System.out.println("total aciertos: " + totac + "\t hip: " + hip + "\t i: " + i);
+          g.setColor(Color.BLUE);
+        }
+        g.fillOval(centroX + X, centroY + Y, 6, 6);
+
+      }
+
+      System.out.println("totac: " + totac + "\tn: " + dardos);
+      System.out.println("PI ~ " + ((double) totac / (double) dardos) * 4);
+    }
+    skip++;
+  }
+
+  public void paint(Graphics g) {
+    DefaultPieDataset dataset = new DefaultPieDataset();
+    // super.paint(g);
+    Graphics2D g2d = (Graphics2D) g;
+    g2d.drawLine(0, 200, 800, 200); // Dibuja las lineas divisoras
+    g2d.drawLine(0, 400, 800, 400);
+    g2d.drawLine(0, 600, 800, 600);
+    g2d.drawLine(0, 800, 800, 800);
+    int contador_a = 0; // Contador de líneas que tocan
+    for (int i = 0; i < n; i++) {
+      int[] coordenadas = generarCoordenadasAleatorias();
+      dibujaAguja(g2d, coordenadas[0], coordenadas[1], coordenadas[2], coordenadas[3]);
+
+      if (coordenadas[1] / 200 != coordenadas[3] / 200) {
+        contador_a++;
+      }
+    }
+    // Grafica de pastel
+    dataset.setValue("Aciertos[" + contador_a + "]", contador_a);
+    dataset.setValue("No Aciertos[" + n + "]", n);
+
+    JFreeChart pieChart = ChartFactory.createPieChart("LANZAMIENTO DE AGUJAS DE BUFFON", dataset);
+
+    JFrame frame = new JFrame("Gráfica de pastel");
+    frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    frame.add(new ChartPanel(pieChart));
+    frame.pack();
+    frame.setLocationRelativeTo(null);
+    double comprobante = (2 * n) / contador_a;
+    if (skipAguja == 2) {
+      frame.setVisible(true);
+      resultados(contador_a, comprobante, n);
+    }
+    skipAguja++;
+
+  }
+
+  private int[] generarCoordenadasAleatorias() {
+    int x1 = 0, x2 = 0, y1 = 0, y2 = 0, longitud = 0;
+    do {
+      x1 = (int) (Math.random() * getWidth());
+      y1 = (int) (Math.random() * getHeight());
+      x2 = (int) (Math.random() * getWidth());
+      y2 = (int) (Math.random() * getHeight());
+      longitud = (int) (Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)));
+    } while (longitud != 200);
+    return new int[] { x1, y1, x2, y2 };
+  }
+
+  private void dibujaAguja(Graphics2D g2d, int x1, int y1, int x2, int y2) {
+    g2d.setColor(Color.blue);
+    g2d.drawLine(x1, y1, x2, y2);
+  }
+
+  public static void resultados(double contador_a, double comprobante, double n) {
+    System.out.println("Las agujas que chocan son:" + contador_a);
+    System.out.println("2*" + n + "/" + contador_a + "=" + comprobante);
+  }
+
+  public static void main(String[] args) {
+    int op = 0;
+    while (op != 3) {
+      System.out.print("======= QUE OBJETO DESEA LANZAR? =======\n1.- Agujas\n2.- Dardos\n3.- Salir\nInput: ");
+      op = leer.nextInt();
+      switch (op) {
+        case 1:
+          agujas();
+          break;
+        case 2:
+          dardos();
+          break;
+        case 3:
+          System.err.println("FIN DEL PROGRAMA :)");
+          break;
+        default:
+          System.err.println("Error: valor incorrecto, ingrese un valor entre 1 y 3");
+          break;
+      }
+    }
+    leer.close(); // Cierra el Scanner después de usarlo
+  }
+
+  public static void dardos() {
+    System.out.print("Cuantos dardos desea lanzar? ");
+    dardos = leer.nextInt();
+
+    JFrame frame = new JFrame("Tablero de Dardos");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.add(new DardosBuffon());
+    frame.setSize(600, 600);
+    frame.setLocationRelativeTo(null);
+    frame.setVisible(true);
+  }
+
+  public static void agujas() {
+    System.out.println("Cuantas agujas desea tirar?");
+    int n = leer.nextInt(); // Cierra el Scanner después de usarlo
+
+    bufon panel = new bufon(n);
+    mostrarPanel(panel);
+  }
+
+  private static void mostrarPanel(JPanel panel) {
+    JFrame frame = new JFrame("Simulación de agujas de bufon");
+    frame.add(panel);
+    frame.pack();
+    frame.setVisible(true);
+  }
+}
