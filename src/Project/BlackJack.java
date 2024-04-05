2168f151e5fb325690d797945f4ab6d6519b3123
YovaInz <yovanniinzunza@gmail.com>
2024-04-04 23:54:35 -0700

    BlackJack.java snapshot0.3 (falta graficación)

--- src/Project/BlackJack.java
+++ src/Project/BlackJack.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class BlackJack {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("cuantos jugadores desea ingresar? ");
         int players = input.nextInt();
         int[][] handPlayer = new int[players][5];
         char[] maso = { 'A', 'A', 'A', 'A', '1', '1', '1', '1', '2', '2', '2', '2', '3', '3', '3', '3', '4', '4', '4',
@@ -27,11 +28,11 @@ public class BlackJack {
             System.out.println(
                     "Jugador #" + (i + 1) + ": [" + (char) handPlayer[i][0] + "][" + (char) handPlayer[i][1] + "]");
         }
-        int turn = 0, stay = 0, house = 0, winNumber;
+        int turn = 0, stay = 0, house = 0;
         int[] newCard = new int[players], hand = new int[players];
         boolean[] toStay = new boolean[players], out = new boolean[players];
         boolean houseWin = true;
-        while (stay != players) {
+        while (stay != players && turn <= 5) {
             turn++;
             for (int i = 0; i < players; i++) {
                 if (toStay[i] || out[i])
@@ -50,9 +51,13 @@ public class BlackJack {
                     toStay[i] = true;
                 }
                 if (hand[i] < 19) {
-                    handPlayer[i][turn] = maso[(int) (Math.random() * 52)];
+                    handPlayer[i][turn] = maso[(int) (Math.random() * 50)];
                     newCard[i] = convertCard(handPlayer, i, turn);
-                    hand[i] += newCard[i];
+                    if (hand[i] == 20 && newCard[i] == 11) {
+                        hand[i]++;
+                    } else {
+                        hand[i] += newCard[i];
+                    }
                     System.out.println("-nueva carta: " + (char) handPlayer[i][turn] + " \n-nueva mano: " + hand[i]);
                 }
                 if (hand[i] >= 19 && hand[i] <= 21 && toStay[i] != true) {
@@ -61,41 +66,48 @@ public class BlackJack {
                     System.out.println("Se planta");
                 }
                 if (hand[i] > 21) {
-                    System.out.println("perdió el jugador #" + (i + 1));
+                    System.err.println("perdió el jugador #" + (i + 1));
                     out[i] = true;
                     stay++;
                 }
             }
         }
-
+        System.out.println("\n=============RESUMEN DE JUEGO=============");
         for (int i = 0; i < players; i++) {
             if (toStay[i])
                 System.out.println("jugador #" + (i + 1) + " se quedó con " + hand[i]);
             else if (out[i])
                 System.out.println("jugador #" + (i + 1) + " se pasó con " + hand[i]);
         }
-
-        while (house < 20) {
+        System.out.println("===========================================\nTurno de la casa");
+        while (house < 21) {
             char cardHouse = maso[(int) (Math.random() * 52)];
+            int value = 0;
             switch (cardHouse) {
                 case 65:
-                    house += 11;
+                    value = 11;
                     break;
                 case 49, 50, 51, 52, 53, 54, 55, 56, 57:
-                    house += Integer.parseInt(cardHouse + "");
+                    value = Integer.parseInt(cardHouse + "");
                     break;
                 case 74, 75, 81:
-                    house += 10;
+                    value = 10;
                 default:
                     break;
             }
-            System.out.println(cardHouse + " : " + house);
+            if (house == 20 && value == 11) {
+                house++;
+            } else {
+                house += value;
+            }
+            System.out.println("- Carta nueva: " + cardHouse + "\n- mano actual " + house + "\n");
         }
+        System.out.println("===========================================");
         if (house == 21)
             System.out.println("LA CASA GANA");
         if (house < 21) {
             for (int i = 0; i < players; i++) {
-                if (hand[i] >= house) {
+                if (hand[i] >= house && hand[i] <= 21) {
                     System.out.println("GANA JUGADOR #" + (i + 1));
                     houseWin = false;
                 }
@@ -103,8 +115,19 @@ public class BlackJack {
             if (houseWin)
                 System.out.println("LA CASA GANA CON " + house);
         }
+        int winner = hand[0];
         if (house > 21) {
-            System.out.println("LA CASA SE PASÓ");
+            System.out.println("LA CASA SE PASÓ\n=================GANADORES=================");
+            for (int i = 0; i < players; i++) {
+                if (hand[i] <= 21 && Math.abs(hand[i] - 21) <= Math.abs(winner - 21)) {
+                    winner = hand[i];
+                }
+            }
+            for (int i = 0; i < players; i++) {
+                if (winner == hand[i])
+                    System.out.println("GANA EL JUGADOR #" + (i + 1));
+            }
+
         }
         input.close();
     }
